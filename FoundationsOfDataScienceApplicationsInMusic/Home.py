e5563510be5368644781e9af1f7d89cebeb53e4b
abbylloyd03 <abbylloyd03@gmail.com>
2025-12-04 14:43:42 -0600

    Update home

--- FoundationsOfDataScienceApplicationsInMusic/Home.py
+++ FoundationsOfDataScienceApplicationsInMusic/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Data & AI Music Guide", page_icon="🎵", layout="wide")
 
-st.title("The Beginner's Guide to Data Science & AI Applications in Music")
+st.title("The Foundational Guide to Data Science & AI Applications in Music")
 st.markdown("""
 This app provides introductory tutorials on open-source analytics and AI tools for music generation and analysis. These tutorials are intended to help music students gain fundamental skills needed to understand current AI applications in music.
 Explore how to use technologies like music21 for music generation and analysis.
